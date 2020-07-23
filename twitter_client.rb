b7a3c7632dff12fed2567d4ebb4cd180023a2ce6
nanaka0012 <nanaka0012@yahoo.co.jp>
2020-07-23 15:41:09 +0900

    add: twitter_client.rb

---
+++ twitter_client.rb
@@ -0,0 +1,20 @@
+require "twitter"
+require "dotenv"
+Dotenv.load
+
+class TwitterClient
+
+  def initialize()
+    @client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV["CONSUMER_KEY"]
+      config.consumer_secret     = ENV["CONSUMER_SECRET"]
+      config.access_token        = ENV["ACCESS_TOKEN"]
+      config.access_token_secret = ENV["ACCESS_TOKEN_SECRET"]
+    end
+  end
+
+  def tweet(str)
+    @client.update(str)
+  end
+
+end
