43eae54486a3107da8da03befe8b699915cd88c4
楊明哲 <mingzhe.yang@hcomp.cs.tsukuba.ac.jp>
2020-07-27 06:49:17 +0000

    FIX: もどしたら治った google api 0.32.1

--- google_api.rb
+++ google_api.rb
@@ -7,20 +7,16 @@ Dotenv.load
 API_KEY=ENV['GOOGLE_API_KEY']
 CSE_ID=ENV['SEARCH_ENGINE_ID']#カスタムサーチAPI
 
-searcher = Google::Apis::CustomsearchV1::CustomsearchService.new
-searcher.key = API_KEY
+custom_search_api = Google::Apis::CustomsearchV1::CustomsearchService.new
+custom_search_api.key = API_KEY
 
 query = {
   num: 3,
   start: 1,
   cr: 25,
   cx: CSE_ID
-  q: "スパイスカレー"
 }
 
-hash=JSON.parse(json)
+json = custom_search_api.list_cses("ビーフストロガノフ",query).to_json
 
-File.open("result_#{query["q"]}.json", 'w') do |file|
-  str = JSON.dump(hash, file)
-end
-puts hash
+puts json
