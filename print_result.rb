e74256660cbfb1eb090b58e10bfcd06216117a27
楊明哲 <mingzhe.yang@hcomp.cs.tsukuba.ac.jp>
2020-07-27 07:06:33 +0000

    ADD: recommend message func in twitter api class

--- print_result.rb
+++ print_result.rb
@@ -5,7 +5,7 @@ File.open(filename) do |file|
     hash=JSON.load(file)
     hash["items"].each do |item|
         puts "title : #{item["title"]}\n" 
-        puts "URL : #{item["htmlFormattedUrl"]}"
-        puts "ImagesURL : #{item["pagemap"]["cse_image"][0]["src"]}\n"
+        # puts "URL : #{item["htmlFormattedUrl"]}"
+        # puts "ImagesURL : #{item["pagemap"]["cse_image"][0]["src"]}\n"
     end
 end
