50e73be5aab84c7b2a38a7573bbc517478eb0e40
KindMaple <angelattack9759@gmail.com>
2020-07-28 14:26:40 +0900

    UPDATE: 「暇」ツイートがないときのエラー処理を追加

--- main.rb
+++ main.rb
@@ -3,19 +3,15 @@ require './google_api'
 require './firestore_client'
 
 # Twitterインスタンス
-# 暇ツイート取得の動作確認
 twitter_client = TwitterClient.new()
 boredTweets = twitter_client.getBoredTweets(2)
-boredTweets.each do |tweet|
+if boredTweets.empty?
+  puts "暇ツイートはありません．"
+  puts "みんな忙しそう"
+else
+  boredTweets.each do |tweet|
     puts tweet.user.screen_name
     puts tweet.text
-end
-twitter_client.setRecentTweet(boredTweets[0].id)
-
-# Firestoreの動作確認
-firestore_client = FirestoreClient.new()
-tweets = twitter_client.getReplies
-tweets.each do | tweet | 
-    firestore_client.saveUser(tweet)
-    firestore_client.saveTweet(tweet)
+  end
+  twitter_client.setRecentTweet(boredTweets[0].id)
 end
