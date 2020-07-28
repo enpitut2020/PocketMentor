d357da83e7b62ba45ccc2537cfb1867bf9e83962
楊明哲 <mei28aquarius@gmail.com>
2020-07-28 18:59:09 +0900

    ADD: misc

--- main.rb
+++ main.rb
@@ -7,46 +7,39 @@ require './firestore_client'
 twitter_client = TwitterClient.new()
 firestore_client = FirestoreClient.new()
 
-tweets = twitter_client.getReplies
 
-tweets.each do | tweet | 
-    firestore_client.saveUser(tweet)
-    firestore_client.saveTweet(tweet)
-end
-
-tweets.each do | tweet | 
-    firestore_client.getTweets(tweet.user.id).each do |users_tweet|
-        puts users_tweet
-    end
-end
-
-SLEEP_TIME = 2 * 60
+SLEEP_TIME = 30
 
 loop {
-  # 暇ツイートパート
-  boredTweets = twitter_client.getBoredTweets()
-  if boredTweets.empty?
-    puts "暇ツイートはありません．"
-    puts "みんな忙しそう"
-  else
-    boredTweets.each do |tweet|
-      #暇と呟いたユーザーのツイートたち　の配列
-      firestore_client.getUserAllWishTweet(tweet.user.id)
-      # ツイート渡す
-      # やりたいことを1こ受け取る
-      # 記事をmention
-      # puts tweet.user.screen_name
-      # puts tweet.text
-    end
-    twitter_client.setRecentTweet(boredTweets[0].id) unless boredTweets[0].nil?
-  end
+  # # 暇ツイートパート
+  # boredTweets = twitter_client.getBoredTweets()
+  # if boredTweets.empty?
+  #   puts "暇ツイートはありません．"
+  #   puts "みんな忙しそう"
+  # else
+  #   boredTweets.each do |tweet|
+  #     #暇と呟いたユーザーのツイートたち　の配列
+  #     firestore_client.getUserAllWishTweet(tweet.user.id)
+  #     # ツイート渡す
+  #     # やりたいことを1こ受け取る
+  #     # 記事をmention
+  #   end
+  #   twitter_client.setRecentTweet(boredTweets[0].id) unless boredTweets[0].nil?
+  # end
 
   # メンション処理パート
-#  replies=getReplies(count)
-#  replies.each do |reply|
-#   twitter_client.formatReply(reply)
-#  end
+  # リプライを取得
+  replies= twitter_client.getReplies()
+  twitter_client.setRecentReply(replies[0].id) unless replies[0].nil?
 
+  replies.each do |reply|
+    #リプライをfirestoreに保存
+    firestore_client.saveUser(reply)
+    firestore_client.saveTweet(reply)
+
+    #保存しましたみたいなことをリプする。
+    twitter_client.sendSavingMessage(reply)
+  end
   # スリープ
   sleep(SLEEP_TIME)
 }
