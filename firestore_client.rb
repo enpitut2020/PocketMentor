05e42d07f04c8ea5049e917de30ee228d547f333
Ni Ryogo <niryogo@enuainoiMac.local>
2020-07-28 14:31:20 +0900

    ADD ユーザーIDからツイート全てゲット

--- firestore_client.rb
+++ firestore_client.rb
@@ -7,6 +7,9 @@ require "google/cloud/firestore"
 
 class FirestoreClient
 
+  # インスタンスを作成
+  # @param [nil] 
+  # @return [nil]
   def initialize()    
     @firestore_client = Google::Cloud::Firestore.new(
       project_id: "pocketmentor-455ae",
@@ -14,8 +17,11 @@ class FirestoreClient
     )
   end
 
-  #ない場合は新規作成される
-  #ある場合は更新される
+  # ない場合は新規作成される
+  # ある場合は更新される
+  # users/tweet_id(不変ID)/ここ更新
+  # @param [Object] Tweetオブジェクト 
+  # @return [nil]
   def saveUser(tweet)
     doc_ref = @firestore_client.doc "users/#{tweet.user.id}"
     doc_ref.set(
@@ -23,6 +29,11 @@ class FirestoreClient
     )
   end
 
+  # ~したいの最初のツイートを保存
+  # ない場合は新規作成されるある場合は更新される
+  # users/twitter_id/tweets/tweetのID/ここを更新
+  # @param [Object] Tweetオブジェクト 
+  # @return [nil]
   def saveTweet(tweet)
     tweet_ref = @firestore_client.doc "users/#{tweet.user.id}/tweets/#{tweet.user.id}"
     tweet_ref.set(
@@ -30,12 +41,32 @@ class FirestoreClient
     )
   end
 
-  #botのリプライを保存する
-  #リプライ先のツイートの直に下保存する
+  # botのリプライを保存する
+  # リプライ先のツイートの直に下保存する
+  # users/twitter_id/tweets/tweetのID/replies/ここを更新
+  # @param [Object] Tweetオブジェクト 
+  # @return [nil]
   def saveReply(tweet)
-    reply_ref = @firestore_client.doc "users/#{tweet.user.id}/tweets/#{tweet.user.id}/replies/aaaa"
+    reply_ref = @firestore_client.doc "users/#{tweet.user.id}/tweets/#{tweet.user.id}/replies/#{tweet.id}"
     reply_ref.set(
       tweet: tweet.text,
     )
   end
+
+
+  # ユーザーが呟いた全てのやりたいことツイート
+  # リプライ先のツイートの直に下保存する
+  # @param [user_id] user_id (tweet.user.id)不変のid
+  # @return [Object] firestoreに保存してるツイートオブジェクト
+  # tweet.data →{tweet: "tweet text"}
+  def getTweets(user_id)
+    tweets_ref = @firestore_client.col "users/#{user_id}/tweets"
+    tweets=[]
+    tweets_ref.get do |tweet|
+      # puts "#{tweet.document_id} data: #{tweet.data}."
+      tweets.push(tweet.data)
+    end
+    return tweets
+  end
+
 end
--- main.rb
+++ main.rb
@@ -10,12 +10,20 @@ boredTweets.each do |tweet|
     puts tweet.user.screen_name
     puts tweet.text
 end
-twitter_client.setRecentTweet(boredTweets[0].id)
+puts boredTweets
+twitter_client.setRecentTweet(boredTweets[0].id) unless boredTweets[0].nil?
 
 # Firestoreの動作確認
 firestore_client = FirestoreClient.new()
 tweets = twitter_client.getReplies
+
 tweets.each do | tweet | 
     firestore_client.saveUser(tweet)
     firestore_client.saveTweet(tweet)
+end
+
+tweets.each do | tweet | 
+    firestore_client.getTweets(tweet.user.id).each do |users_tweet|
+        puts users_tweet.data
+    end
 end
