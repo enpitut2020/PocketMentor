dbcfde2c27d13d1833d8f1d6ad114cf8a4f6290e
KindMaple <angelattack9759@gmail.com>
2020-07-28 14:42:41 +0900

    UPDATE: 2分おきに「暇」ツイートを取得するようにした

--- main.rb
+++ main.rb
@@ -2,16 +2,22 @@ require './twitter_client'
 require './google_api'
 require './firestore_client'
 
-# Twitterインスタンス
-twitter_client = TwitterClient.new()
-boredTweets = twitter_client.getBoredTweets(2)
-if boredTweets.empty?
-  puts "暇ツイートはありません．"
-  puts "みんな忙しそう"
-else
-  boredTweets.each do |tweet|
-    puts tweet.user.screen_name
-    puts tweet.text
+SLEEP_TIME = 2 * 60
+
+loop {
+  # Twitterインスタンス
+  twitter_client = TwitterClient.new()
+  boredTweets = twitter_client.getBoredTweets()
+  if boredTweets.empty?
+    puts "暇ツイートはありません．"
+    puts "みんな忙しそう"
+  else
+    boredTweets.each do |tweet|
+      puts tweet.user.screen_name
+      puts tweet.text
+    end
+    twitter_client.setRecentTweet(boredTweets[0].id)
   end
-  twitter_client.setRecentTweet(boredTweets[0].id)
-end
+
+  sleep(SLEEP_TIME)
+}
