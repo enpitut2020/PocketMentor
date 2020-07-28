ae7c04fc10add2375c4e9b8a814e656bea80ac0f
Ni Ryogo <niryogo@enuainoiMac.local>
2020-07-28 11:07:11 +0900

    ADD firestore_clientでツイートとユーザー保存の実行

--- main.rb
+++ main.rb
@@ -1,5 +1,12 @@
 require './twitter_client'
+require './firestore_client'
+twitter_client = TwitterClient.new()
+# twitter_client.sendReplies(2)
 
-client = TwitterClient.new()
-
-client.sendReplies(2)
+firestore_client = FirestoreClient.new()
+tweets = twitter_client.getReplies
+# puts tweets
+tweets.each do | tweet | 
+    firestore_client.saveUser(tweet)
+    firestore_client.saveTweet(tweet)
+end
