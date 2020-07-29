1b6b6065da57f7e307d3ca5e7fd372cb74ae4b97
Katsuya Suzuki <angelattack9759@gmail.com>
2020-07-29 11:09:25 +0900

    UPDATE: misc

--- main.rb
+++ main.rb
@@ -6,25 +6,30 @@ twitter_client = TwitterClient.new()
 firestore_client = FirestoreClient.new()
 
 
-SLEEP_TIME = 30
+SLEEP_TIME = 1 * 60
 
 loop {
   # # 暇ツイートパート
+  twitter_client.reloadRecentTweetId()
   boredTweets = twitter_client.getBoredTweets()
   if boredTweets.empty?
     puts "暇ツイートはありません．"
     puts "みんな忙しそう"
   else
     twitter_client.setRecentTweet(boredTweets[0].id) unless boredTweets[0].nil?
-    boredTweets.each do |tweet|
-      recent_wish_doc = firestore_client.getUserRecentWishTweet(tweet.user.id)
-      message = twitter_client.createRecommendMessageFromText(recent_wish_doc[:tweet])
-      reply_message = twitter_client.addUserName(message, tweet.user.screen_name)
-      twitter_client.mention(reply_message, tweet.id)
+    boredTweets.reverse_each do |tweet|
+      # recent_wish_doc = firestore_client.getUserRecentWishTweet(tweet.user.id)
+      # message = twitter_client.createRecommendMessageFromText(recent_wish_doc[:tweet])
+      # reply_message = twitter_client.addUserName(message, tweet.user.screen_name)
+      # twitter_client.mention(reply_message, tweet.id)
+      puts tweet.id
+      puts tweet.user.screen_name
+      puts tweet.text + "\n"
     end
   end
   # メンション処理パート
   # リプライを取得
+  twitter_client.reloadRecentReplyId()
   replies= twitter_client.getReplies()
   twitter_client.setRecentReply(replies[0].id) unless replies[0].nil?
 
